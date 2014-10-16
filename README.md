86da799b255012117e8bfa731cc2337e9e8dff5a
nanakochi123456 <yy845jp@gmail.com>
2014-10-16 17:11:32 +0900

    1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+sakuramml_smhitomi3_h3_5
+=========================
+
+SM調教師瞳3 h3_5 瞳の歌
+
+http://www.nicovideo.jp/watch/sm24430679
---
+++ h3_5.bat
@@ -0,0 +1,4 @@
+y:
+cd "Program\Timidity++\Timidity++"
+start timidity -c ../patches/cfgs/h3_5.cfg -Ow --output-stereo -s 48000 --reverb=d,127 e:\bgm\h3_5\h3_5.mid
+timidity -c ../patches/cfgs/h3_5.cfg --output-stereo -s 48000 --reverb=d,127 e:\bgm\h3_5\h3_5.mid
