76946878dbcc97c86cc75aab7b82fc680faf2437
nanoko <yy845jp@gmail.com>
2014-11-18 13:25:57 +0900

    change directory

---
+++ src/1config.bat
@@ -0,0 +1,12 @@
+rem Å´à»â∫ÇÇ©Ç¶ÇƒÇ›ÇÈ
+SET TIMIDITYHOME=e:\Timidity++
+rem SET TIMIDITYHOME=c:\t\Timidity++
+SET INPUT=e:\home\Ç‰Ç¡ÇΩ\documents\github\sakuramml_smhitomi3_h3_5\src
+SET OUTPUT=e:\home\Ç‰Ç¡ÇΩ\documents\github\sakuramml_smhitomi3_h3_5\bin
+rem Å™Ç±Ç±Ç‹Ç≈
+
+SET TIMIDITYBIN=%TIMIDITYHOME%\Timidity++
+SET TIMIDITYPAT=%TIMIDITYHOME%\Patches
+SET TIMIDITYCFG=%TIMIDITYHOME%\Patches\cfgs
+
+SET TIMIDITYRCFGPATH=../patches/cfgs
---
+++ src/project.bat
@@ -0,0 +1,6 @@
+SET TITLE=SM’²‹³Žt“µ3 “µ‚Ì‰Ì
+SET CONFIG=h3_5.cfg
+SET PLAYOPTION=--output-stereo --sampling-freq=44100 -EFchorus=n,10 -EFreverb=d,127 --output-16bit
+SET OPTION=--output-stereo --sampling-freq=44100 -EFchorus=n,10 -EFreverb=d,127 --output-24bit --volume=90, --drum-power=90
+SET MIDI1=h3_5.mid
+SET MIDI1PLAY=1
